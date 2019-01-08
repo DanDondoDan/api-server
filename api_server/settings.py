18c91444c8029263abfc650a5e2798890080f2ca
GenBor <Generalovboris@outlook.com>
2019-01-08 19:48:17 +0200

    init app

--- api_server/settings.py
+++ api_server/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ENV.DJANGO_SECRET_KEY
-
+# SECRET_KEY = ENV.DJANGO_SECRET_KEY
+SECRET_KEY = '8wtex-zbw)avw4q$)!!&2*n0^8!wux-d9s!t#x^h%ei!e7hr9n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = ENV.DEBUG
 TOKEN_LIFETIME = ENV.TOKEN_DURATION_HOURS
